3252ec11b4f1d0be2e12656b141614f1d550dae0
Guilherme Goncalves <grgoncal@gmail.com>
2022-01-29 19:15:01 -0300

    First test added

---
+++ HackerNews.UnitTests/IoC.cs
@@ -0,0 +1,91 @@
+﻿using HackerNews.API;
+using HackerNews.API.Application.Services.Cache;
+using HackerNews.Domain.Entities.Base;
+using HackerNews.Domain.Interfaces.App.Services.Cache;
+using HackerNews.Domain.Interfaces.Infra.DataAccess.Redis;
+using HackerNews.Domain.Interfaces.Infra.DataAccess.Redis.Factory;
+using HackerNews.Domain.Interfaces.Infra.Logger;
+using HackerNews.Domain.Interfaces.Infra.Services.HackerNews;
+using HackerNews.Infraestructure.DataAccess.Redis;
+using HackerNews.Infraestructure.DataAccess.Redis.Factory;
+using HackerNews.Infraestructure.Logger;
+using HackerNews.Infraestructure.Services.HackerNews;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+using NUnit.Framework;
+using System;
+
+namespace HackerNews.Tests
+{
+    [SetUpFixture]
+    public class IoC
+    {
+        public static IServiceCollection ServiceCollection { get; set; }
+        public static IServiceProvider ServiceProvider { get; set; }
+
+        [OneTimeSetUp]
+        public void AssemblyInit()
+        {
+            ServiceCollection = new ServiceCollection();
+            ServiceCollection.AddScoped<IMediator, Mediator>();
+
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                                                   .AddEnvironmentVariables()
+                                                   .Build();
+            ServiceCollection.AddSingleton<IConfiguration>(configuration);
+            ServiceCollection.Configure<AppSettings>(configuration);
+
+            ServiceCollection.AddMvc();
+            ServiceCollection.AddControllers();
+
+            ServiceCollection.AddMediatR(typeof(Startup));
+            ServiceCollection.AddMediatR(typeof(IRequestHandler<>));
+            ServiceCollection.AddMediatR(typeof(IRequestHandler<,>));
+            ServiceCollection.AddMediatR(typeof(INotificationHandler<>));
+
+            ServiceCollection.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            ServiceCollection.TryAddSingleton<IActionContextAccessor, ActionContextAccessor>();
+
+            ServiceCollection.AddSingleton<ILogger, Logger>();
+
+            ServiceCollection.AddTransient<IHackerNewsClient, HackerNewsClient>();
+            ServiceCollection.AddTransient<IHackerNewsService, HackerNewsService>();
+
+            ServiceCollection.AddSingleton<INewsCacheService, NewsCacheService>();
+
+            ServiceCollection.AddSingleton<IRedisConnectionFactory, RedisConnectionFactory>();
+            ServiceCollection.AddSingleton<IHackerNewsRedis, HackerNewsRedis>();
+
+            ServiceProvider = ServiceCollection.BuildServiceProvider();
+        }
+        
+        public static IHackerNewsService GetHackerNewsRestClient()
+        {
+            return ServiceProvider.GetService<IHackerNewsService>();
+        }
+
+        public static IMediator GetMediator()
+        {
+            return ServiceProvider.GetService<IMediator>();
+        }
+
+        public static ControllerContext SetControllerContext()
+        {
+            return new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+        }
+
+        internal static IOptions<AppSettings> GetSettings()
+        {
+            return ServiceProvider.GetService<IOptions<AppSettings>>();
+        }
+    }
+}
---
+++ HackerNews.UnitTests/UnitTesting/HackerNews/REST/HackerNewsRESTTest.cs
@@ -0,0 +1,56 @@
+﻿using HackerNews.Domain.Entities.Base;
+using HackerNews.Domain.Interfaces.Infra.Services.Base;
+using HackerNews.Domain.Interfaces.Infra.Services.HackerNews;
+using HackerNews.Infraestructure.Services.HackerNews;
+using Microsoft.Extensions.Options;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerNews.Tests.UnitTesting.HackerNews.REST
+{
+    public class HackerNewsRestTest
+    {
+        private IHackerNewsService _hackerNewsService;
+        private IOptions<AppSettings> _settings;
+
+        private Mock<IHackerNewsClient> _hackerNewsClientMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            _settings = IoC.GetSettings();
+            _hackerNewsService = IoC.GetHackerNewsRestClient();
+            _hackerNewsClientMock = new Mock<IHackerNewsClient>();
+        }
+
+        [Test]
+        public void When_GetAllStoriesIds_Expect_AllStoriesIds()
+        {
+            var result = _hackerNewsService.GetListOfBestHistoriesIds();
+
+            Assert.IsTrue(result != null);
+            Assert.IsTrue(result.Count > 1);
+        }
+
+        [Test]
+        public void When_GetStoryDetail_Expect_StoryDetail()
+        {
+            var id = _hackerNewsService.GetListOfBestHistoriesIds().FirstOrDefault();
+
+            var detailedNew = _hackerNewsService.GetNewDetails(id);
+
+            Assert.IsTrue(detailedNew != null);
+            Assert.IsTrue(!string.IsNullOrEmpty(detailedNew.Title));
+            Assert.IsTrue(!string.IsNullOrEmpty(detailedNew.By));
+            Assert.IsTrue(detailedNew.Score != null);
+            Assert.IsTrue(detailedNew.Id == id);
+            Assert.IsTrue(detailedNew.Time != null && detailedNew.Time > 0);
+        }
+
+    }
+}
